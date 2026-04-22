7c8288ed64d6877efd8fc6328293f6e65645cf4a
anujakanase <anujakanase99@gmail.com>
2026-04-22 11:32:21 +0530

    Initialize git repo

--- AWS-EC2-VPC-NGINX/ec2.tf
+++ AWS-EC2-VPC-NGINX/ec2.tf
@@ -4,7 +4,6 @@ resource "aws_instance" "nginxserver" {
   subnet_id = aws_subnet.public-subnet.id
   vpc_security_group_ids =  [aws_security_group.nginx-sg.id]
   associate_public_ip_address = true
-
   user_data = <<-EOF
              #!/bin/bash
              sudo yum install nginx -y
